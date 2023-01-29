1e124e1971f7da5815711836e8b4239d9daabc74
Somesh Shukla <somesh13shukla@gmail.com>
2023-01-30 02:29:26 +0530

    File storage

--- server/index.js
+++ server/index.js
@@ -24,6 +24,15 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
+  
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
-
-
+const upload = multer({ storage });
