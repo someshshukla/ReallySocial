ed3590d1004bc9053e57544f68d43a9acd65d60f
Somesh Shukla <somesh13shukla@gmail.com>
2023-01-31 03:22:49 +0530

    MONGOOSE SETUP

--- server/index.js
+++ server/index.js
@@ -51,4 +51,11 @@ const upload = multer({ storage });
 
 
 
-/*MONGOOSE CONFIGURATION */
+/*MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    }).then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    }).catch((error) => console.log(`${error} did not connect`))
