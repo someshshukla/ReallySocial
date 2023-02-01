bf6c3c42e40400f8330a94acdc147b5c7a027535
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-01 22:29:55 +0530

    Authorisation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,3 +3,40 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /* REGISTER USER */
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName, 
+            email,
+            password, 
+            firends,
+            location,
+            occupation,
+
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+               firstName,
+               lastName,
+               email,
+               password: passwordHash,
+               picturePath,
+               firends,
+               location,
+               occupation,
+               viewedProfile: Math.floor(Math.random() * 10000),
+               impressions: Math.floor(Math.random() * 10000)
+
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+
+    } catch(err){
+
+    }
+}
