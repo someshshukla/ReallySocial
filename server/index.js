25ddfbe7e36efb81abdb8911f1a57b0535369848
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-04 18:52:18 +0530

    posts

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from  "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from  "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
  
 
 /* CONFIGURATIONS */
@@ -44,11 +47,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
+app.use("/posts", postRoutes);
 
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
