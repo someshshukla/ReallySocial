53a0757356c1eec0ddd3a224bf6d2b2187f1114f
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-04 17:42:56 +0530

    User Routes

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ import morgan from "morgan";
 import path from  "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js"
 import { register } from "./controllers/auth.js";
-
+ 
 
 /* CONFIGURATIONS */
 
@@ -44,8 +45,10 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 
-/* ROUTES WITH FILES*/
-app.post("/auth/register". upload.single("picture"), register);
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
 
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
