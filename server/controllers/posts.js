ca0ca58b31fb716e663d5f37366ecffd2b8a46b2
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-05 14:11:35 +0530

    post controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,46 @@
 import Post from "../models/Post.js";
+
+/* CREATE */
+export const createPost = async(req, res) => {
+    try{
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post ({
+            userId,
+            firstName: user.firstName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            like: {},
+            comments: []
+        })
+        await newPost.save();
+        const post = await Post.find();
+        res.status(201).json(post)
+
+    } catch (err) {
+        res.status(409).json({ message: err.message})
+    }
+} 
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(202).json(post)
+    } catch (err) {
+        res.status(404).json({ message: err.message})
+
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(202).json(post)
+    } catch (err) {
+        res.status(404).json({ message: err.message})
+
+    }
+}
