6a57e5eed951424379dae9989f1ed8ac45690581
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-05 13:56:17 +0530

    posts

---
+++ server/models/Post.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        location: String,
+        Description: String,
+        userPicturePath: String,
+        likes: {
+            type: Map,
+            of: Boolean,
+
+        },
+        comments: {
+            types: Array,
+            default: []
+        }
+    },
+    {timestamps: true}
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+
+export default Post;
