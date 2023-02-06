57df5a3527e35833eb9412059709638d1b4c06c4
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-07 00:54:30 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from  "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
- 
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
+
 
 /* CONFIGURATIONS */
 
@@ -61,4 +65,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     }).then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+        /* ADD DATA ONE TIME */
+        //User.insertMany(users);
+        //Post.insertMany(posts);
     }).catch((error) => console.log(`${error} did not connect`))
