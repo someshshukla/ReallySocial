21f2b1d8ea38e58d0795e5a3d67b73f696919aef
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-07 01:03:25 +0530

    save

--- server/index.js
+++ server/index.js
@@ -64,8 +64,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser:true,
     useUnifiedTopology: true,
     }).then(() => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
         /* ADD DATA ONE TIME */
-        //User.insertMany(users);
-        //Post.insertMany(posts);
+        User.insertMany(users);
+        Post.insertMany(posts);
     }).catch((error) => console.log(`${error} did not connect`))
