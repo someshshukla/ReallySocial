0c6f42a387fac19bee8c103d1391852287645450
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-07 22:26:50 +0530

    imported redux stuff

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { 
+  persistStore,
+  peristsReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist/lib/storage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
